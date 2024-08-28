9011f934931eb7c0bcef43ff2981f7931c851f53
siri123 <saisirishayarra692@gmail.com>
2024-08-28 15:55:44 +0530

    expense shell project

---
+++ backend.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+LOG_FOLDER=/var/log/expense-backend
+mkdir -p $LOG_FOLDER
+SCRIPT_NAME=$(echo "$0" | cut -d "." -f1)
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE=$LOG_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USER_ID=$(id -u)
+CHECK_ROOT()
+{
+    if [ $USER_ID -ne 0 ]
+    then
+        echo "Please run the script through root access i.e SUDO :"
+        exit 1
+    fi
+}
+
+VALIDATE()
+{
+    if [ $1 -eq 0 ]
+    then 
+        echo -e "$2.... is $G SUCCESS $N"
+    else
+        echo -e "$2 .... is $R FAILED $N"
+        exit 1
+    fi
+}
+CHECK_ROOT
+
+dnf module disable nodejs -y
+VALIDATE $? "Disabling Nodejs "
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "Enabling Nodejs"
+
+dnf install nodejs -y
+VALIDATE $? "Installing Nodejs "
+
+useradd expense
+VALIDATE $? "Adding Expense User"
+
+mkdir -p /app
+VALIDATE $? "Making the directory"
+
+
